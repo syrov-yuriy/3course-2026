c23b44ab4b50a86dff07dbba9530b5815b577b4a
Yuriy Syrov <82206213+syrov-yuriy@users.noreply.github.com>
2026-09-23 14:27:15 +0300

    Начало игры на Реакте

---
+++ src/javascripts/components/AppRps.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App () {
+    return (
+        <div className="container">
+            <div className="answers"><div className="answer">Камень</div><div className="answer">Ножницы</div><div className="answer">Бумага</div></div>
+        </div>
+    )
+}
+
+export default App;
